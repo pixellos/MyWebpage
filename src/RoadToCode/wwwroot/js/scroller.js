88cb68409373ee494d316224b07ff6d6c322f78b
Mateusz Popielarz <rogoznocraft@gmail.com>
2017-05-28 19:42:42 +0200

    Code cleanup

--- src/RoadToCode/wwwroot/js/scroller.js
+++ src/RoadToCode/wwwroot/js/scroller.js
@@ -1,40 +1,13 @@
 "use strict";
 
 let scroller = {}
-
+scroller.containerSelector = '.post'
 scroller.currentContainerSeeker = function (selector) {
     let x = window.innerWidth  / 2;
     let y = window.innerHeight  / 2;
     let middler = $(document.elementFromPoint(x, y));
     let result = middler.closest(selector);
-    console.log("closest:");
-    console.log(result);
     return result;
-}
-scroller.containerSelector = '.post'
-
-scroller.nextHandler = function () {
-    let searched = scroller.currentContainerSeeker(scroller.containerSelector);
-    if (searched.length == 0) {
-        scroller.currentContainerNotFoundCallBack();
-        return;
-    }
-    let nextElement = searched.next();
-    var scrollTo = nextElement.length == 0 ? searched.offset().top + searched.height() : nextElement.offset().top;
-    $('html, body').animate({
-        scrollTop: scrollTo
-    }, 400);
-};
-
-scroller.currentContainerNotFoundCallBack = function () {
-    Materialize.toast("I found nothing :(", 500);
-}
-
-/**
- * @param  {jQuery} element
- */
-scroller.attachNextClick = function (element) {
-    element.click(scroller.nextHandler);
 }
 
 scroller.prevHandler = function () {
@@ -49,7 +22,33 @@ scroller.prevHandler = function () {
         scrollTop: scrollTo
     }, 400);
 };
+scroller.nextHandler = function () {
+    let searched = scroller.currentContainerSeeker(scroller.containerSelector);
+    if (searched.length == 0) {
+        scroller.currentContainerNotFoundCallBack();
+        return;
+    }
+    let nextElement = searched.next();
+    var scrollTo = nextElement.length == 0 ? searched.offset().top + searched.height() : nextElement.offset().top;
+    $('html, body').animate({
+        scrollTop: scrollTo
+    }, 400);
+};
 
+/**
+ * @param  {jQuery} element -Element to attach handler at
+ */
+scroller.attachNextClick = function (element) {
+    element.click(scroller.nextHandler);
+}
+
+/**
+ * @param  {jQuery} element -Element to attach handler at
+ */
 scroller.attachPrevClick = function (element) {
     element.click(scroller.prevHandler)
+}
+
+scroller.currentContainerNotFoundCallBack = function () {
+    Materialize.toast("I found nothing :(", 500);
 }
